43c424f05019c73da40690858ae4b45e4474b63c
John Zila <john@jzila.com>
2015-04-03 15:49:04 -0700

    Moved the chat over to redis pubsub so the whole cluster can chat now

--- demo-server/server.js
+++ demo-server/server.js
@@ -3,9 +3,9 @@ var http = require("http"),
 	express = require("express"),
 	bodyParser = require('body-parser'),
 	multer = require('multer'),
-    async = require('asyncawait/async'),
-    await = require('asyncawait/await'),
-    Promise = require('bluebird'),
+	async = require('asyncawait/async'),
+	await = require('asyncawait/await'),
+	Promise = require('bluebird'),
 	kblib = require("./api/kblib.js"),
 	util = require("./lib/util.js"),
 	constants = require("./constants.js");
@@ -13,30 +13,46 @@ var redis = Promise.promisifyAll(require('node-redis'));
 
 var app = express();
 var port = process.env.PORT || 8084;
-var redisHost = process.env.REDIS_HOST || 'localhost';
 
+
+// Redis session handling
+var redisHost = process.env.REDIS_HOST || 'localhost';
 var redisClient = redis.createClient(6379, redisHost);
+var redisSubscriber = redis.createClient(6379, redisHost);
+redisSubscriber.on("connect", function() {
+	console.log("Redis client ready. Subscribing to " + chatChannel);
+	redisSubscriber.subscribe(chatChannel);
+});
 var redisPrefix = "kb-login-demo:";
 var sessionPrefix = redisPrefix + "sessions:";
 
-// Redis session handling
 var sessions = {};
 var setSessionUser = function(token, user) {
-    var b64 = (new Buffer(JSON.stringify(user))).toString('base64');
-    redisClient.set(sessionPrefix + token, b64);
+	var b64 = (new Buffer(JSON.stringify(user))).toString('base64');
+	redisClient.set(sessionPrefix + token, b64);
 };
 var getSessionUser = async.result(function(token) {
-    var userBase64 = await(redisClient.getAsync(sessionPrefix + token));
-    if (userBase64) {
-        var buf = new Buffer(userBase64.toString(), 'base64');
-        var buf_utf8 = buf.toString('utf8');
-        return JSON.parse(buf_utf8);
-    }
-    return {};
+	var userBase64 = await(redisClient.getAsync(sessionPrefix + token));
+	if (userBase64) {
+		var buf = new Buffer(userBase64.toString(), 'base64');
+		var buf_utf8 = buf.toString('utf8');
+		return JSON.parse(buf_utf8);
+	}
+	return {};
 });
 
 var httpServer = http.Server(app);
 var io = require('socket.io')(httpServer);
+
+var chatChannel = redisPrefix + "chat-channel";
+redisSubscriber.on("message", function(channel, message) {
+	if (channel === chatChannel) {
+		var buf = new Buffer(message.toString(), 'base64');
+		var buf_json = buf.toString('utf8');
+		var obj = JSON.parse(buf_json);
+		io.emit("chat_message", obj);
+	}
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -63,15 +79,15 @@ app.post(/^\/api\/kb_cert_verify\/?$/, async(function (req, resp) {
 	}
 	var signature = req.body.signature;
 	var blob = JSON.parse(req.body.blob);
-    if (blob.token && getSessionUser(blob.token)) {
-        kblib.kbCertVerify(
-            blob,
-            signature,
-            util.makeSendResponse(resp, function(obj) { setSessionUser(obj.user.token, obj.user); })
-        );
-    } else {
-        errResponseCb(400, "Unknown blob identifier");
-    }
+	if (blob.token && getSessionUser(blob.token)) {
+		kblib.kbCertVerify(
+			blob,
+			signature,
+			util.makeSendResponse(resp, function(obj) { setSessionUser(obj.user.token, obj.user); })
+		);
+	} else {
+		errResponseCb(400, "Unknown blob identifier");
+	}
 }));
 
 io.on('connection', function(socket){
@@ -83,16 +99,18 @@ io.on('connection', function(socket){
 		console.log('user disconnected');
 	});
 	socket.on("chat_message", async(function(obj) {
-        var user;
-        if (!obj.token || !(user = getSessionUser(obj.token))) {
-            errHandler("Unrecognized user");
-        } else {
-            var msg = obj.message;
-            if (msg.length > 1024) {
-                msg = msg.substr(0, 1010) + "...[truncated]";
-            }
-            io.emit("chat_message", {user: user.kb_username, message: msg});
-        }
+		var user;
+		if (!obj.token || !(user = getSessionUser(obj.token))) {
+			errHandler("Unrecognized user");
+		} else {
+			var msg = obj.message;
+			if (msg.length > 1024) {
+				msg = msg.substr(0, 1010) + "...[truncated]";
+			}
+			var obj = {user: user.kb_username, message: msg};
+			var obj_base64 = new Buffer(JSON.stringify(obj)).toString('base64');
+			redisClient.publish(chatChannel, obj_base64);
+		}
 	}));
 });
 
