37df4d412e1d509bc766384f92752d94824135b3
John Zila <john@jzila.com>
2015-04-06 16:02:19 -0700

    Attempt to fix socket.io using sticky sessions

--- demo-server/server.js
+++ demo-server/server.js
@@ -6,6 +6,7 @@ var http = require("http"),
 	async = require('asyncawait/async'),
 	await = require('asyncawait/await'),
 	Promise = require('bluebird'),
+    sticky = require('sticky-session'),
 	kblib = require("./api/kblib.js"),
 	util = require("./lib/util.js"),
 	constants = require("./constants.js");
@@ -14,8 +15,10 @@ var redis = Promise.promisifyAll(require('node-redis'));
 var app = express();
 var port = process.env.PORT || 8084;
 
-
 // Redis session handling
+var redisPrefix = "kb-login-demo:";
+var sessionPrefix = redisPrefix + "sessions:";
+var chatChannel = redisPrefix + "chat-channel";
 var redisHost = process.env.REDIS_HOST || 'localhost';
 var redisClient = redis.createClient(6379, redisHost);
 var redisSubscriber = redis.createClient(6379, redisHost);
@@ -23,8 +26,6 @@ redisSubscriber.on("connect", function() {
 	console.log("Redis client ready. Subscribing to " + chatChannel);
 	redisSubscriber.subscribe(chatChannel);
 });
-var redisPrefix = "kb-login-demo:";
-var sessionPrefix = redisPrefix + "sessions:";
 
 var sessions = {};
 var setSessionUser = function(token, user) {
@@ -41,80 +42,81 @@ var getSessionUser = async.result(function(token) {
 	return {};
 });
 
-var httpServer = http.Server(app);
-var io = require('socket.io')(httpServer);
+sticky(function() {
+    var httpServer = http.Server(app);
+    var io = require('socket.io')(httpServer);
 
-var chatChannel = redisPrefix + "chat-channel";
-redisSubscriber.on("message", function(channel, message) {
-	if (channel === chatChannel) {
-		var buf = new Buffer(message.toString(), 'base64');
-		var buf_json = buf.toString('utf8');
-		var obj = JSON.parse(buf_json);
-		io.emit("chat_message", obj);
-	}
-});
+    redisSubscriber.on("message", function(channel, message) {
+        if (channel === chatChannel) {
+            var buf = new Buffer(message.toString(), 'base64');
+            var buf_json = buf.toString('utf8');
+            var obj = JSON.parse(buf_json);
+            io.emit("chat_message", obj);
+        }
+    });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(multer());
 
-app.use(express.static(__dirname + '/static'));
+    app.use(express.static(__dirname + '/static'));
 
-app.get(/^\/api\/get_blob\/?$/, function (req, resp) {
-	var protocol = req.headers.referer.slice(0, req.headers.referer.indexOf("://"));
-	var url = protocol + "://" + req.headers.host + constants.API_KB_VERIFY;
+    app.get(/^\/api\/get_blob\/?$/, function (req, resp) {
+        var protocol = req.headers.referer.slice(0, req.headers.referer.indexOf("://"));
+        var url = protocol + "://" + req.headers.host + constants.API_KB_VERIFY;
 
-	kblib.getBlob(
-		constants.SITE_ID,
-		url,
-		util.makeSendResponse(resp, function(blob) { setSessionUser(blob.token, {}); })
-	);
-});
+        kblib.getBlob(
+            constants.SITE_ID,
+            url,
+            util.makeSendResponse(resp, function(blob) { setSessionUser(blob.token, {}); })
+        );
+    });
 
-app.post(/^\/api\/kb_cert_verify\/?$/, async(function (req, resp) {
-	var errResponseCb = util.makeSendResponse(resp);
-	if (!req.body.blob || !req.body.signature) {
-		console.log("Signature data not valid");
-		return errResponseCb(400, "Invalid signature data");
-	}
-	var signature = req.body.signature;
-	var blob = JSON.parse(req.body.blob);
-	if (blob.token && getSessionUser(blob.token)) {
-		kblib.kbCertVerify(
-			blob,
-			signature,
-			util.makeSendResponse(resp, function(obj) { setSessionUser(obj.user.token, obj.user); })
-		);
-	} else {
-		errResponseCb(400, "Unknown blob identifier");
-	}
-}));
+    app.post(/^\/api\/kb_cert_verify\/?$/, async(function (req, resp) {
+        var errResponseCb = util.makeSendResponse(resp);
+        if (!req.body.blob || !req.body.signature) {
+            console.log("Signature data not valid");
+            return errResponseCb(400, "Invalid signature data");
+        }
+        var signature = req.body.signature;
+        var blob = JSON.parse(req.body.blob);
+        if (blob.token && getSessionUser(blob.token)) {
+            kblib.kbCertVerify(
+                blob,
+                signature,
+                util.makeSendResponse(resp, function(obj) { setSessionUser(obj.user.token, obj.user); })
+            );
+        } else {
+            errResponseCb(400, "Unknown blob identifier");
+        }
+    }));
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-	var errHandler = function(err) {
-		socket.emit("chat_error", err);
-	};
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-	socket.on("chat_message", async(function(obj) {
-		var user;
-		if (!obj.token || !(user = getSessionUser(obj.token))) {
-			errHandler("Unrecognized user");
-		} else {
-			var msg = obj.message;
-			if (msg.length > 1024) {
-				msg = msg.substr(0, 1010) + "...[truncated]";
-			}
-			var obj = {user: user.kb_username, message: msg};
-			var obj_base64 = new Buffer(JSON.stringify(obj)).toString('base64');
-			redisClient.publish(chatChannel, obj_base64);
-		}
-	}));
-});
+    io.on('connection', function(socket){
+        console.log('a user connected');
+        var errHandler = function(err) {
+            socket.emit("chat_error", err);
+        };
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+        socket.on("chat_message", async(function(obj) {
+            var user;
+            if (!obj.token || !(user = getSessionUser(obj.token))) {
+                errHandler("Unrecognized user");
+            } else {
+                var msg = obj.message;
+                if (msg.length > 1024) {
+                    msg = msg.substr(0, 1010) + "...[truncated]";
+                }
+                var obj = {user: user.kb_username, message: msg};
+                var obj_base64 = new Buffer(JSON.stringify(obj)).toString('base64');
+                redisClient.publish(chatChannel, obj_base64);
+            }
+        }));
+    });
 
-httpServer.listen(port, function() {
-	console.log("listening on " + port);
+    return httpServer;
+}).listen(port, function() {
+    console.log("listening on " + port);
 });
 
