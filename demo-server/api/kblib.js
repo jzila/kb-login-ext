638f66a8168c7ec49bdebcf61197c7e5e1ea5395
John Zila <john@jzila.com>
2015-05-28 21:14:32 -0700

    Refactored demo server to use asyncawait. Fixes #12

--- demo-server/api/kblib.js
+++ demo-server/api/kblib.js
@@ -3,9 +3,12 @@ var exports = module.exports = {};
 var http = require("http"),
 	https = require("https"),
 	assert = require("assert"),
-	kbpgp = require("kbpgp"),
 	crypto = require("crypto"),
+	async = require('asyncawait/async'),
+	await = require('asyncawait/await'),
+	Promise = require('bluebird'),
 	util = require("../lib/util.js");
+var kbpgp = Promise.promisifyAll(require("kbpgp"));
 
 var pkey_username_url = "https://keybase.io:443/_/api/1.0/user/lookup.json?usernames={0}&fields=basics,profile,public_keys";
 var pkey_fingerprint_url = "https://keybase.io:443/_/api/1.0/user/lookup.json?key_fingerprint={0}&fields=basics,profile,public_keys";
@@ -34,45 +37,67 @@ var validateSignature = function (blob, blobFromSignature) {
     return true;
 };
 
-var makeKeyManagerCallback = function (blob, signature, user, cb) {
-	return function (err, km) {
-		if (!err && km) {
-			var ring = new kbpgp.keyring.KeyRing;
-			ring.add_key_manager(km);
-			kbpgp.unbox({keyfetch: ring, armored: signature }, function (err, literals) {
-				if (!err) {
-					var decryptedSignature = literals[0].toString();
-					var blobFromSignature = JSON.parse(decryptedSignature);
-                    if (validateSignature(blob, blobFromSignature)) {
-                        var user_name = "";
-                        var location = "";
-                        if (user['profile']) {
-                            var profile = user['profile'];
-                            user_name = profile['full_name'] || user_name;
-                            location = profile['location'] || location;
-                        }
-						cb(200, {
-							status: {code: 0, name: "OK"},
-							user: {
-								kb_username: user['basics']['username'],
-								kb_uid: user['id'],
-								full_name: user_name,
-								location: location,
-								token: blob.token
-							}
-						});
-                    } else {
-						cb(400, "Mismatched blob and signature");
-                    }
-				} else {
-					cb(400, "Unable to verify signature");
-				}
-			});
-		} else {
-			cb(400, "Unable to load public key");
-		}
-	};
+var validatePublicData = function(publicData) {
+    return publicData &&
+           publicData.status &&
+           publicData.status.name === "OK" &&
+           publicData.them &&
+           publicData.them.length &&
+           publicData.them[0].public_keys &&
+           publicData.them[0].public_keys.primary &&
+           publicData.them[0].public_keys.primary.bundle;
 };
+
+var handleKbCertVerify = async(function(publicData, blob, signature, cb) {
+    try {
+        if (!validatePublicData(publicData)) {
+            throw "Error obtaining matching public key";
+        }
+        var user = publicData.them[0];
+        var kms, km, ring, literals;
+        try {
+            kms = await(kbpgp.KeyManager.import_from_armored_pgpAsync({armored: user.public_keys.primary.bundle}));
+        } catch (err) {
+            throw "Unable to load public key";
+        }
+        if (!kms) {
+            throw "Unable to load key manager";
+        }
+        km = kms[0];
+        ring = new kbpgp.keyring.KeyRing;
+        ring.add_key_managerAsync(km);
+        try {
+            literals = await(kbpgp.unboxAsync({keyfetch: ring, armored: signature}));
+        } catch(err) {
+            throw "Unable to verify signature";
+        }
+        var decryptedSignature = literals[0].toString();
+        var blobFromSignature = JSON.parse(decryptedSignature);
+        if (!validateSignature(blob, blobFromSignature)) {
+            throw "Mismatched blob and signature";
+        }
+        var user_name = "",
+            location = "";
+        if (user['profile']) {
+            var profile = user['profile'];
+            user_name = profile['full_name'] || user_name;
+            location = profile['location'] || location;
+        }
+        cb(200, {
+            status: {code: 0, name: "OK"},
+            user: {
+                kb_username: user['basics']['username'],
+                kb_uid: user['id'],
+                full_name: user_name,
+                location: location,
+                token: blob.token
+            }
+        });
+    } catch (err) {
+        console.log("Error: " + err);
+        cb(400, err);
+    }
+});
 
 exports.kbCertVerify = function (blob, signature, cb) {
 	if (!validateBlob(blob)) {
@@ -88,23 +113,8 @@ exports.kbCertVerify = function (blob, signature, cb) {
 		});
 
 		response.on('end', function () {
-			var publicData = JSON.parse(body);
-			if (publicData &&
-				publicData.status &&
-				publicData.status.name === "OK" &&
-				publicData.them &&
-				publicData.them.length &&
-				publicData.them[0].public_keys &&
-				publicData.them[0].public_keys.primary &&
-				publicData.them[0].public_keys.primary.bundle) {
-				var user = publicData.them[0];
-				kbpgp.KeyManager.import_from_armored_pgp(
-					{armored: user.public_keys.primary.bundle},
-					makeKeyManagerCallback(blob, signature, user, cb)
-				);
-			} else {
-				cb(400, "Error obtaining matching public key");
-			}
+            var publicData = JSON.parse(body);
+            handleKbCertVerify(publicData, blob, signature, cb);
 		});
 	};
 
